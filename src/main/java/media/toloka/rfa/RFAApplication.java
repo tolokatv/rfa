d2f98f44bfb33dacae80c297481e44262dc82805
ysv <ysv@toloka.kiev.ua>
2024-01-12 20:31:05 +0200

    Додав гарний опис системи безпеки Spring Security

--- src/main/java/media/toloka/rfa/RFAApplication.java
+++ src/main/java/media/toloka/rfa/RFAApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RFAApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RFAApplication.class, args);
 	}
