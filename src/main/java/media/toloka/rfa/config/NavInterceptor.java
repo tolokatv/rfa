000215bb994c6809f63e79f19a20e7c5c8f3447d
ysv <ysv@toloka.kiev.ua>
2024-01-25 20:44:27 +0200

    Додав message про кількість нових повідомлень до форм Документи, Угоди та Станції

--- src/main/java/media/toloka/rfa/config/NavInterceptor.java
+++ src/main/java/media/toloka/rfa/config/NavInterceptor.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
