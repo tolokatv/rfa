c2f718369c77939dcca8755db2f86eb455d9a825
ysv <ysv@pp-ua.org>
2024-01-31 07:48:07 +0200

    Додав збереження результату кожного кроку виконання завдання через RPC. Забув про перетворення у Gson з урахуванням @Expose. Виправляюся.

--- src/main/java/media/toloka/rfa/rpc/model/ResultJob.java
+++ src/main/java/media/toloka/rfa/rpc/model/ResultJob.java
@@ -1,10 +1,13 @@
 package media.toloka.rfa.rpc.model;
 
+import com.google.gson.annotations.Expose;
 import lombok.Data;
 
 @Data
 public class ResultJob {
+    @Expose
     private Long rc;
+    @Expose
     private ERPCJobType job;
 
     public ResultJob (Long rc, ERPCJobType job) {
