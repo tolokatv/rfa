bfca76f8e66448f070dbfd5ddc4416e92b1f2345
ysv <ysv@toloka.kiev.ua>
2024-02-04 19:43:26 +0200

    Пробуємо вичитати стан станції на сторінці Прибрали зайві повідомлення з консолі Додав глобальну змінну js stationId, яка заповнюється шаблонізатором

--- src/main/resources/static/js/rfa.js
+++ src/main/resources/static/js/rfa.js
@@ -12,7 +12,7 @@ let intervalId;
 window.addEventListener('DOMContentLoaded', event => {
 
 async function getStatus() {
-    let siteurl = "https://rfa.toloka.media/api/1.0/ps/1352"+stationId;
+    let siteurl = "https://rfa.toloka.media/api/1.0/ps/"+stationId;
     console.log(siteurl);
   const response = await fetch(siteurl);
   const data = await response.json();
