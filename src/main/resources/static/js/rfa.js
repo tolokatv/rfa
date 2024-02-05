e23f1727bcc98c3e61c85d8b5d36cd8adb43ca9d
ysv <ysv@toloka.kiev.ua>
2024-02-05 10:56:11 +0200

    Додав у рест кількість запущених сервісів

--- src/main/resources/static/js/rfa.js
+++ src/main/resources/static/js/rfa.js
@@ -20,7 +20,6 @@ async function getStatus() {
   const response = await fetch(siteurl);
   const data = await response.json();
 //  console.log(data);
-//  console.log(stationId);
 
     console.log(data);
     stationstate = data["status"];
@@ -29,15 +28,15 @@ async function getStatus() {
 
     switch (stationstate) {
         case 6:
-            myspan.value = 'Всі сервіси працюють нормально.';
+            myspan.textContent = 'Всі сервіси працюють нормально.';
             myspan.setAttribute("style", "color:green;");
             break;
         case 0:
-            myspan.value = 'Станція вимкнена';
+            myspan.textContent = 'Станція вимкнена';
             myspan.setAttribute("style", "color:red;");
             break;
         default:
-            myspan.value = 'Станція в процесі запуску або зупинки';
+            myspan.textContent = 'Станція в процесі запуску або зупинки';
             myspan.setAttribute("style", "color:orange;");
             break;
     }
