a200d525fb1543fc59e4e5da349afc470a3f9994
ysv <ysv@toloka.kiev.ua>
2024-02-05 14:08:30 +0200

    Закінчив обробку кнопок в залежності від стану станції

--- src/main/resources/static/js/rfa.js
+++ src/main/resources/static/js/rfa.js
@@ -34,12 +34,8 @@ function getkeyhrev () {
 
 async function getStatus() {
     let siteurl = "https://rfa.toloka.media/api/1.0/ps/"+stationId;
-//    console.log(siteurl);
-  const response = await fetch(siteurl);
-  const data = await response.json();
-//  console.log(data);
-
-    console.log(data);
+    const response = await fetch(siteurl);
+    const data = await response.json();
     stationstate = data["status"];
     myspan = document.getElementById('spanstationstate');
 
