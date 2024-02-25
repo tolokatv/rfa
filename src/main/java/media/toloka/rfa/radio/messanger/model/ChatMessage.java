36b6a9d2627d320b6613f44fcaff3728df71f956
ysv <ysv@pp-ua.org>
2024-02-25 16:01:31 +0200

    Почав робити онлайн обмін сповіщень через Stomp

---
+++ src/main/java/media/toloka/rfa/radio/messanger/model/ChatMessage.java
@@ -0,0 +1,28 @@
+package media.toloka.rfa.radio.messanger.model;
+
+import com.google.gson.annotations.Expose;
+import lombok.Data;
+import media.toloka.rfa.radio.model.Clientdetail;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+public class ChatMessage {
+    @Expose
+    private String uuid= UUID.randomUUID().toString();
+    @Expose
+    private Date send;
+    @Expose
+    private String fromname;
+    @Expose
+    private String fromuuid;
+    @Expose
+    private String toname;
+    @Expose
+    private String touuid;
+    private Clientdetail to; // to user
+    private Clientdetail from; // from user
+    @Expose
+    private String body;
+}
