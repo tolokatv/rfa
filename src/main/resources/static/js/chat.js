d17eef4af45da7705924deaa1db37c272127d079
ysv <ysv@toloka.kiev.ua>
2024-03-04 09:37:43 +0200

    Розбираюся з Firefox Начебто працює чат... начебто ....

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -28,8 +28,8 @@ var colors = [
 ];
 function connect() {
     //username = document.querySelector('#name').value.trim();
-    var socket = new WebSocket("/rfachat");
-//    var socket = new WebSocket("ws://rfa.toloka.media/rfachat");
+//    var socket = new WebSocket("/rfachat");
+    var socket = new WebSocket("ws://rfa.toloka.media/rfachat");
 //    var socket = new SockJS('/rfachat');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, onConnected, onError);
