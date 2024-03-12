780736a71ebd79f3a9f4b74e3d3c4f26da88d72a
ysv <ysv@toloka.kiev.ua>
2024-03-12 12:33:24 +0200

    Свічу запит до сервера. Можливо варто перейти на фільтри замість HandlerInterceptor

--- src/main/java/media/toloka/rfa/config/interceptor/LoggerInterceptor.java
+++ src/main/java/media/toloka/rfa/config/interceptor/LoggerInterceptor.java
@@ -2,9 +2,13 @@ package media.toloka.rfa.config.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class LoggerInterceptor implements HandlerInterceptor {
+
+    final Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
@@ -14,6 +18,7 @@ public class LoggerInterceptor implements HandlerInterceptor {
             remoteAddr = request.getRemoteAddr();
         }
         // todo Логировать обращения к серверу.
+        logger.info(request.getRequestURL().toString());
 //        System.out.println(remoteAddr);
         return true;
     }
