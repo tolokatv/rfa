be8846dd751ed556dfea1ee309095a391ba0eb72
ysv <ysv@toloka.kiev.ua>
2024-05-04 14:05:29 +0300

    Намагаюся зробити карусель.

--- src/main/java/media/toloka/rfa/radio/creater/repository/AlbumCoverRepository.java
+++ src/main/java/media/toloka/rfa/radio/creater/repository/AlbumCoverRepository.java
@@ -3,6 +3,7 @@ package media.toloka.rfa.radio.creater.repository;
 
 import media.toloka.rfa.radio.model.Albumсover;
 import media.toloka.rfa.radio.model.Clientdetail;
+import media.toloka.rfa.radio.store.model.EStoreFileType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,5 +12,6 @@ public interface AlbumCoverRepository extends JpaRepository<Albumсover, Long> {
 
 //    Optional
     List<Albumсover> findByClientdetail(Clientdetail cd);
+    //List<Albumсover> findByClientdetailAnd(Clientdetail cd, EStoreFileType sft);
     Albumсover getById(Long id);
 }
