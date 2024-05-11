1c7db62cd73397340edf6e3d2522fa0d7347847d
ysv <ysv@toloka.kiev.ua>
2024-05-11 16:15:44 +0300

    Попередня структура для RSS подкасту

--- src/main/java/media/toloka/rfa/radio/podcast/model/ChannelPodcast.java
+++ src/main/java/media/toloka/rfa/radio/podcast/model/ChannelPodcast.java
@@ -1,5 +1,12 @@
 package media.toloka.rfa.radio.podcast.model;
 
 public class ChannelPodcast {
-
+    private String title;
+    private String link;
+    private String description;
+    private String lastBuildDate;
+    private String language;
+    private ImagePodcast image;
+    private String copyright;
+    private ItemPodcast[] item;
 }
--- src/main/java/media/toloka/rfa/radio/podcast/model/ItemPodcast.java
+++ src/main/java/media/toloka/rfa/radio/podcast/model/ItemPodcast.java
@@ -1,5 +1,12 @@
 package media.toloka.rfa.radio.podcast.model;
 
 public class ItemPodcast {
+    private String title;
+    private String link;
+    private String pubDate;
+    private String comments;
+    private String category;
+    private String description;
+    private String enclosure;
 
 }
