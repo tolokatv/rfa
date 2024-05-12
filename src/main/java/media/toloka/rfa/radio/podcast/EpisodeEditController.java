af827de8ac66ef90bc383d8fae41a6632aa1e045
ysv <ysv@toloka.kiev.ua>
2024-05-12 20:56:58 +0300

    Копирсаюся з подкастом. Попередньо зробив структуру процесу створення подкасту

---
+++ src/main/java/media/toloka/rfa/radio/podcast/EpisodeEditController.java
@@ -0,0 +1,27 @@
+package media.toloka.rfa.radio.podcast;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class EpisodeEditController {
+
+    final Logger logger = LoggerFactory.getLogger(PodcastController.class);
+
+    @GetMapping(value = "/podcast/episodeedit/{puuid}")
+    public String podcastroot(
+            @PathVariable String puuid,
+            Model model ) {
+        logger.info("Зайшли на /podcast/episodedit/{puuid}");
+
+
+        return "/podcast/episodeedit";
+    }
+
+
+}
--- src/main/java/media/toloka/rfa/radio/podcast/model/PodcastImage.java
+++ src/main/java/media/toloka/rfa/radio/podcast/model/PodcastImage.java
@@ -3,6 +3,7 @@ package media.toloka.rfa.radio.podcast.model;
 import com.google.gson.annotations.Expose;
 import jakarta.persistence.*;
 import lombok.Data;
+import media.toloka.rfa.radio.store.model.Store;
 
 import java.util.UUID;
 
@@ -18,4 +19,8 @@ public class PodcastImage {
     private String title;
     private String url;
     private String link;
+
+    @OneToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "store_id")
+    private Store storeid;
 }
