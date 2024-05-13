134d4a9e29226530c7766a044fb080c0485fa9cb
ysv <ysv@toloka.kiev.ua>
2024-05-13 11:34:27 +0300

    Копирсаюся з подкастом. Пишу логіку для процесу створення подкасту та форми

--- src/main/java/media/toloka/rfa/radio/podcast/model/PodcastItem.java
+++ src/main/java/media/toloka/rfa/radio/podcast/model/PodcastItem.java
@@ -3,7 +3,9 @@ package media.toloka.rfa.radio.podcast.model;
 import com.google.gson.annotations.Expose;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -14,13 +16,29 @@ public class PodcastItem {
     @GeneratedValue
     @Expose
     private Long id;
+    @Expose
     private String uuid = UUID.randomUUID().toString();
+    @Expose
     private String title;
+    @Expose
     private String link;
+    @Expose
     private String pubDate;
+    @Expose
     private String comments;
+    @Expose
     private String category;
+    @Expose
+    @Column(columnDefinition = "TEXT")
     private String description;
+    @Expose
     private String enclosure;
+    @Expose
+    @DateTimeFormat(pattern = "dd-MM-yy")
+    private Date date = new Date();
 
+    @ElementCollection
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "podcast_channel_id")
+    private PodcastChannel chanel;
 }
--- src/main/java/media/toloka/rfa/radio/podcast/service/PodcastService.java
+++ src/main/java/media/toloka/rfa/radio/podcast/service/PodcastService.java
@@ -1,10 +1,13 @@
 package media.toloka.rfa.radio.podcast.service;
 
+import media.toloka.rfa.radio.model.Clientdetail;
 import media.toloka.rfa.radio.podcast.model.PodcastChannel;
 import media.toloka.rfa.radio.podcast.model.PodcastItem;
 import media.toloka.rfa.radio.podcast.repositore.ChanelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PodcastService {
@@ -18,5 +21,9 @@ public class PodcastService {
 
     public void SavePodcast(PodcastChannel podcast) {
         chanelRepository.save(podcast);
+    }
+
+    public List<PodcastChannel> GetPodcastListByCd(Clientdetail cd) {
+        return chanelRepository.findByClientdetail(cd);
     }
 }
