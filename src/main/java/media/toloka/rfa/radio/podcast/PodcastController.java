e7f66fd9fbfef05a311e133636b604143d918351
ysv <ysv@toloka.kiev.ua>
2024-05-21 16:29:24 +0300

    Розібралися з призначенням обкладинки для альбому

--- src/main/java/media/toloka/rfa/radio/podcast/PodcastController.java
+++ src/main/java/media/toloka/rfa/radio/podcast/PodcastController.java
@@ -5,6 +5,7 @@ import media.toloka.rfa.radio.client.service.ClientService;
 import media.toloka.rfa.radio.model.Clientdetail;
 import media.toloka.rfa.radio.model.Station;
 import media.toloka.rfa.radio.podcast.model.PodcastChannel;
+import media.toloka.rfa.radio.podcast.model.PodcastItem;
 import media.toloka.rfa.radio.podcast.service.PodcastService;
 import media.toloka.rfa.security.model.Users;
 import org.slf4j.Logger;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
@@ -53,6 +55,29 @@ public class PodcastController {
         }
 
         return "/podcast/home";
+    }
+
+    @GetMapping(value = "/podcast/view/{puuid}")
+    public String podcastview(
+            @PathVariable String puuid,
+            Model model ) {
+        logger.info("Зайшли на /podcast/view/{puuid}");
+//        Users user = clientService.GetCurrentUser();
+//        if (user == null) {
+//            return "redirect:/";
+//        }
+//        Clientdetail cd = clientService.GetClientDetailByUser(clientService.GetCurrentUser());
+//        if (cd == null) { return "redirect:/"; }
+
+
+
+        PodcastChannel podcastChannel = podcastService.GetChanelByUUID(puuid);
+        List<PodcastItem> podcastItems = podcastChannel.getItem();
+
+        model.addAttribute("podcastChannel",  podcastChannel);
+        model.addAttribute("podcastItems",  podcastItems);
+
+        return "/podcast/view";
     }
 
     @PostMapping(value = "/podcast/home")
