7c0bd6b6135e93ffa99ade62f41d3665e5252806
ysv <ysv@toloka.kiev.ua>
2024-06-05 15:06:57 +0300

    Додав bash файли для роботи

--- src/main/resources/ShellFiles/RFAstart.sh
+++ src/main/resources/ShellFiles/RFAstart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/rfa/prodact
+
 git pull
+cp ~/prodact/src/main/resources/ShellFiles/* ~/bin/
 ./gradlew build -x test
 /bin/sh -c 'until ping -c1 8.8.8.8; do sleep 5; done;'
 /bin/sh -c 'until ping -c4 pg.rfa; do sleep 1; done;'
