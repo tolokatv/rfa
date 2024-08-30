abdbf9f9122587dd12dcd9e781713163ec9fa62e
ysv <ysv@toloka.kiev.ua>
2024-08-30 20:14:51 +0300

    Вишукую перелік uuid епізодів подкасту

--- src/main/java/media/toloka/rfa/radio/root/ViewTrack.java
+++ src/main/java/media/toloka/rfa/radio/root/ViewTrack.java
@@ -2,6 +2,7 @@ package media.toloka.rfa.radio.root;
 
 import media.toloka.rfa.radio.creater.service.CreaterService;
 import media.toloka.rfa.radio.model.Track;
+import media.toloka.rfa.radio.store.Service.StoreService;
 import media.toloka.rfa.radio.store.model.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -24,6 +25,13 @@ public class ViewTrack {
             Model model) {
 
         Track curtrack = createrService.GetTrackByUuid(trackuuid);
+        // to do виправляю помилку. Прибрати коли зникнуть null у посилання на Store_id
+//        if (curtrack.getStoreitem() == null) {
+//            curtrack.setStoreitem(createrService.GetStoreByStoreuuid(curtrack.getStoreuuid()));
+//            createrService.SaveTrack(curtrack);
+//        }
+        // to do виправляю помилку. Прибрати коли зникнуть null у посилання на Store_id
+
         model.addAttribute("curtrack", curtrack );
         return "/guest/viewtrack";
     }
